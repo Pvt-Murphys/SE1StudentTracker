b8b139ae888620c833c275f515037ba5f29b2fd0
Mason <kelleymt@etsu.edu>
2025-11-02 17:27:46 -0500

    Added javascript fetch API to not reload page on clock in

--- obj/Debug/net8.0/SE1StudentTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/SE1StudentTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SE1StudentTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75fd97b3a1397551b159c2a20bf7e4d380333fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba695d2bd5710c90816f19934e7bc8d40a54424f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SE1StudentTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SE1StudentTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
