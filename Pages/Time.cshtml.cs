9951081da8f7aea80b54f439936f1bcd323c9738
RandomFurret <garbear713@gmail.com>
2025-11-16 12:03:10 -0500

    time now clocks in and out with the user's email correctly

--- Pages/Time.cshtml.cs
+++ Pages/Time.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using SE1StudentTracker.Services;
 using System.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace SE1StudentTracker.Pages
 {
@@ -11,9 +13,9 @@ namespace SE1StudentTracker.Pages
     {
         private readonly Services.OracleService _oracleService;
         public DataTable QueryResults { get; set; }
-
         [BindProperty]
         public string Location { get; set; }
+
 
         public DateTime Clock_in_at = DateTime.Now;
 
@@ -24,21 +26,26 @@ namespace SE1StudentTracker.Pages
 
         public IActionResult OnPostClockIn()
         {
-            
-            _oracleService.ExecuteCreateUpdateDelete($"INSERT INTO TIME_SESSION (USER_ID, SESSION_TYPE, LOCATION_TEXT, CLOCK_IN_AT) VALUES ('', 'Clinical', '{Location}', SYSTIMESTAMP)");
-            
+            string username = this.User.Identity.Name;
+            Console.WriteLine(username);
+            _oracleService.ExecuteCreateUpdateDelete(
+                $"INSERT INTO TIME_SESSION (USER_ID, SESSION_TYPE, LOCATION_TEXT, CLOCK_IN_AT) VALUES ('{username}', 'Clinical', '{Location}', SYSTIMESTAMP)");
+
             return RedirectToPage();
         }
 
         public IActionResult OnPostClockOut()
         {
-            _oracleService.ExecuteCreateUpdateDelete("UPDATE TIME_SESSION SET CLOCK_OUT_AT = SYSTIMESTAMP WHERE USER_ID = 1");
+            string username = this.User.Identity.Name;
+            _oracleService.ExecuteCreateUpdateDelete(
+                $"UPDATE TIME_SESSION SET CLOCK_OUT_AT = SYSTIMESTAMP WHERE USER_ID = '{username}'");
 
             return RedirectToPage();
         }
+
         public void OnGet()
         {
-            QueryResults = _oracleService.ExecuteQuery("SELECT STUDENT_NUMBER FROM STUDENT_PROFILE");
-        }
+            QueryResults = _oracleService.ExecuteQuery("SELECT STUDENT_NUMBER FROM STUDENT_PROFILE"); 
+        } 
     }
 }
